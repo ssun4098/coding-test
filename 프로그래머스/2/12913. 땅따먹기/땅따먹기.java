aff4f788a4f909592fcbd6c8fed2291c39f9a5f3
김선홍 <bebo007@naver.com>
2026-02-01 16:08:28 +0900

    [level 2] Title: 땅따먹기, Time: 28.99 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -0,0 +1,24 @@
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int [land.length][4];
+        dp[0][0] = land[0][0];
+        dp[0][1] = land[0][1];
+        dp[0][2] = land[0][2];
+        dp[0][3] = land[0][3];
+        
+        for(int i = 1; i < land.length; i++) {
+            dp[i][0] = Math.max(Math.max(dp[i - 1][1],  dp[i - 1][2]), dp[i - 1][3]) + land[i][0];
+            dp[i][1] = Math.max(Math.max(dp[i - 1][0],  dp[i - 1][2]), dp[i - 1][3]) + land[i][1];
+            dp[i][2] = Math.max(Math.max(dp[i - 1][0],  dp[i - 1][1]), dp[i - 1][3]) + land[i][2];
+            dp[i][3] = Math.max(Math.max(dp[i - 1][0],  dp[i - 1][1]), dp[i - 1][2]) + land[i][3];
+        }
+        
+        
+        for(int i = 0; i < 4; i++) {
+            answer = Math.max(answer, dp[land.length - 1][i]);
+        }
+        
+        return answer;
+    }
+}
