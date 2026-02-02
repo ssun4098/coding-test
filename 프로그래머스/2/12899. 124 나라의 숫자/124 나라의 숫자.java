326be28ba261f9d333c2052a4d9006ec61581f28
김선홍 <bebo007@naver.com>
2026-02-02 19:51:34 +0900

    [level 2] Title: 124 나라의 숫자, Time: 4.73 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        while(n > 0) {
+            if (n % 3 == 0) {
+                answer = "4" + answer;
+                n = n / 3 - 1;
+            } else if (n % 3 == 1) {
+                answer = "1" + answer;
+                n /= 3;
+            } else {
+                answer = "2" + answer;
+                n /= 3;
+            }
+        }
+        return answer;
+    }
+}
