b62497cb9117f762f2422bf55a94c35df8eb4fb0
김선홍 <bebo007@naver.com>
2026-03-18 17:47:30 +0900

    [Silver II] Title: DNA 비밀번호, Time: 304 ms, Memory: 24436 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -6,86 +6,66 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer firstInput = new StringTokenizer(br.readLine(), " ");
-        StringTokenizer secondInput = new StringTokenizer(br.readLine());
-        StringTokenizer thirdInput = new StringTokenizer(br.readLine(), " ");
+
+        StringTokenizer firstInput = new StringTokenizer(br.readLine());
+        String dna = br.readLine();
+        StringTokenizer thirdInput = new StringTokenizer(br.readLine());
 
         int s = Integer.parseInt(firstInput.nextToken());
         int p = Integer.parseInt(firstInput.nextToken());
-        String dna = secondInput.nextToken();
+
         int[] acgt = new int[4];
         int[] cache = new int[4];
-        int answer = 0;
+
         for(int i = 0; i < 4; i++) {
             acgt[i] = Integer.parseInt(thirdInput.nextToken());
         }
-        int end = p;
-        char[] chars = dna.toCharArray();
 
+        char[] chars = dna.toCharArray();
+        int answer = 0;
+
+        // 초기 윈도우
         for(int i = 0; i < p; i++) {
-            char c = chars[i];
-            add(cache, c);
+            add(cache, chars[i]);
         }
 
-        while(end <= s) {
-            boolean flag = true;
-            for(int i = 0; i < 4; i++) {
-                if(cache[i] < acgt[i]) {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
+        for(int end = p; end <= s; end++) {
+
+            if(isValid(cache, acgt)) {
                 answer++;
             }
-            if(end < s) {
-                char remove = chars[end - p];
-                char add = chars[end];
-                remove(cache, remove);
-                add(cache, add);
-            }
-            end++;
+
+            if(end == s) break;
+
+            remove(cache, chars[end - p]);
+            add(cache, chars[end]);
         }
-
-
 
         bw.write(String.valueOf(answer));
         bw.flush();
     }
 
+    private static boolean isValid(int[] cache, int[] acgt) {
+        for(int i = 0; i < 4; i++) {
+            if(cache[i] < acgt[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private static void add(int[] array, char c) {
-        if(c == 'A') {
-            array[0]++;
-        }
-
-        if(c == 'C') {
-            array[1]++;
-        }
-
-        if(c == 'G') {
-            array[2]++;
-        }
-
-        if(c == 'T') {
-            array[3]++;
-        }
+        array[index(c)]++;
     }
 
     private static void remove(int[] array, char c) {
-        if(c == 'A') {
-            array[0]--;
-        }
+        array[index(c)]--;
+    }
 
-        if(c == 'C') {
-            array[1]--;
-        }
-
-        if(c == 'G') {
-            array[2]--;
-        }
-
-        if(c == 'T') {
-            array[3]--;
-        }
+    private static int index(char c) {
+        if(c == 'A') return 0;
+        if(c == 'C') return 1;
+        if(c == 'G') return 2;
+        return 3; // T
     }
 }
