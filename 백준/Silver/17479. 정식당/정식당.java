cd0a42415b11f3abaa3efe7cbcaf52d0847e4245
김선홍 <bebo007@naver.com>
2026-03-31 04:37:17 +0900

    [Silver III] Title: 정식당, Time: 556 ms, Memory: 66128 KB -BaekjoonHub

--- 백준/Silver/17479. 정식당/정식당.java
+++ 백준/Silver/17479. 정식당/정식당.java
@@ -4,86 +4,122 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static String NO = "No";
-    private static String OKAY = "Okay";
+
+    private static final String NO = "No";
+    private static final String OKAY = "Okay";
+
+    enum FoodType {
+        NORMAL,
+        SPECIAL,
+        SERVICE
+    }
+
+    static class Food {
+
+        private FoodType type;
+        private int price;
+
+        public Food(FoodType type, int price) {
+            this.type = type;
+            this.price = price;
+        }
+
+        public boolean isNormal() {
+            return type == FoodType.NORMAL;
+        }
+
+        public boolean isSpecial() {
+            return type == FoodType.SPECIAL;
+        }
+
+        public boolean isService() {
+            return type == FoodType.SERVICE;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
+
         try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
+            Map<String, Food> menu = new HashMap<>();
 
-            Map<String, Integer> normal = new HashMap<>();
-            Map<String, Integer> special = new HashMap<>();
-            Map<String, Integer> service = new HashMap<>();
-
-            input(a, normal, br);
-            input(b, special, br);
-            input(c, service, br);
+            input(a, menu, br, FoodType.NORMAL);
+            input(b, menu, br, FoodType.SPECIAL);
+            input(c, menu, br, FoodType.SERVICE);
 
             int n = Integer.parseInt(br.readLine());
-            String[] orders = new String[n];
+
+            long normalSum = 0;
+            long specialSum = 0;
+
+            int specialCount = 0;
+            int serviceCount = 0;
+
             for(int i = 0; i < n; i++) {
-                orders[i] = br.readLine();
+
+                String order = br.readLine();
+
+                Food food = menu.get(order);
+
+                if(food.isNormal()) {
+                    normalSum += food.getPrice();
+                }
+                else if(food.isSpecial()) {
+                    specialSum += food.getPrice();
+                    specialCount++;
+                }
+                else if(food.isService()) {
+                    serviceCount++;
+                }
             }
 
-            System.out.println(result(normal, special, service, orders));
+            System.out.println(
+                    validate(normalSum, specialSum, specialCount, serviceCount)
+            );
         }
     }
 
-    public static void input(int n, Map<String, Integer> map, BufferedReader br) throws IOException {
+    public static void input(int n,
+                             Map<String, Food> menu,
+                             BufferedReader br,
+                             FoodType type) throws IOException {
+
         for(int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
             String name = st.nextToken();
+
             int price = 0;
-            if(st.hasMoreTokens()) {
+
+            if(type != FoodType.SERVICE) {
                 price = Integer.parseInt(st.nextToken());
             }
 
-            map.put(name, price);
+            menu.put(name, new Food(type, price));
         }
     }
 
-    public static String result(Map<String, Integer> normal,
-                                Map<String, Integer> special,
-                                Map<String, Integer> service,
-                                String[] orders) {
-        long normalSumPrice = 0;
+    public static String validate(long normalSum,
+                                  long specialSum,
+                                  int specialCount,
+                                  int serviceCount) {
 
-        long specialSumPrice = 0;
-        int specialCount = 0;
-
-        int serviceCount = 0;
-
-        for(String order: orders) {
-            Integer normalPrice = normal.getOrDefault(order, -1);
-            Integer specialPrice = special.getOrDefault(order, -1);
-            Integer servicePrice = service.getOrDefault(order, -1);
-
-            if(normalPrice > 0) {
-                normalSumPrice += normalPrice;
-                continue;
-            }
-
-            if(specialPrice > 0) {
-                specialSumPrice += specialPrice;
-                specialCount++;
-                continue;
-            }
-
-
-            if(servicePrice > -1) {
-                serviceCount++;
-            }
-        }
-
-        if(specialCount > 0 && normalSumPrice < 20000) {
+        if(specialCount > 0 && normalSum < 20000) {
             return NO;
         }
 
-        if(serviceCount > 0 && normalSumPrice + specialSumPrice < 50000) {
+        if(serviceCount > 0 && normalSum + specialSum < 50000) {
             return NO;
         }
 
