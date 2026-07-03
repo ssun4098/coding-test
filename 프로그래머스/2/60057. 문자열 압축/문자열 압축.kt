992c33b5031f600825e39eae01e3046bb59b8c1a
김선홍 <bebo007@naver.com>
2026-07-03 10:24:23 +0900

    [level 2] Title: 문자열 압축, Time: 10.52 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.kt
@@ -1,32 +1,27 @@
 class Solution {
+fun appendGroup(sb: StringBuilder, count: Int, word: String) {
+    if (count > 1) sb.append(count)
+    sb.append(word)
+}
+
 fun solution(s: String): Int {
     var answer = s.length
 
     for(i in 1 until s.length - 1) {
         val chunked = s.chunked(i)
         var count = 0
-        var last = ""
+        var last: String? = null
         val sb = StringBuilder()
         for(word in chunked) {
-            if(word != last) {
-                if(last != "") {
-                    if(count == 1) {
-                        sb.append(last)
-                    } else {
-                        sb.append("$count$last")
-                    }
-                }
-                count = 1
-                last = word
-            } else {
+            if(word == last) {
                 count++
+                continue
             }
+            last?.let { appendGroup(sb, count, it) }
+            count = 1
+            last = word
         }
-        if(count == 1) {
-            sb.append(last)
-        } else {
-            sb.append("$count$last")
-        }
+        appendGroup(sb, count, last!!)
         answer = minOf(answer, sb.length)
     }
 
